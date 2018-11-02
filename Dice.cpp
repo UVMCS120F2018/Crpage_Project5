31d6e92f669f948d800c0c29c139aa8d5c91fe0f
cecilyp <cecilyrbpage@gmail.com>
2018-11-02 16:04:53 -0400

    created dice class

---
+++ Dice.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Cecily Page on 11/2/18.
+//
+
+#include "Dice.h"
+
+Dice::Dice(int faces, int number) {
+    this -> faces = faces;
+    this -> number = number;
+}
+
+int Dice::roll() {
+    int total = 0;
+    for(int i=0; i<number; i++){
+        total = total + rand()%6;
+    }
+    return total;
+}
---
+++ Dice.h
@@ -0,0 +1,21 @@
+//
+// Created by Cecily Page on 11/2/18.
+//
+
+#ifndef CRPAGE_PROJECT5_DICE_H
+#define CRPAGE_PROJECT5_DICE_H
+
+
+class Dice {
+
+    int faces;
+    int number;
+
+public:
+    Dice(int faces, int number);
+    int roll();
+
+};
+
+
+#endif //CRPAGE_PROJECT5_DICE_H
